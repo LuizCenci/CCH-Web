2d63090c3809556ee81960194c41654d6987d969
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2025-02-10 17:23:49 -0300

    hidden api key

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiKey = 'AIzaSyAfH5HUi8XmzgcvQ3JzkT5-6GlpbtdG0sg'
+const address = "Rua das Graças 1040";
+const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(address)}`;
+document.getElementById("map-frame").src = mapUrl;
+
+const imgs = document.querySelectorAll('.img-menor');
+imgs.forEach(img => {
+img.addEventListener("mouseover", function () {
+    const imgExibida = document.getElementById('imgExibida');
+    imgExibida.src = img.src; 
+});
+});
