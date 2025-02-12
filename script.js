c936a3b7f9d6d76e9d39c42ff7be3b590a4eaac1
LuizCenci <155825996+LuizCenci@users.noreply.github.com>
2025-02-12 18:01:11 -0300

    novo layout

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const address = "Rua das Graças 1040";
 const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(address)}`;
 document.getElementById("map-frame").src = mapUrl;
 
+
 const imgs = document.querySelectorAll('.img-menor');
-imgs.forEach(img => {
-img.addEventListener("mouseover", function () {
-    const imgExibida = document.getElementById('imgExibida');
-    imgExibida.src = img.src; 
-});
-});
+    imgs.forEach(img => {
+    img.addEventListener("mouseover", function () {
+        const imgExibida = document.getElementById('imgExibida');
+        imgExibida.src = img.src; 
+    });
+    }); 
